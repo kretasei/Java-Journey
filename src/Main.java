b893923c98e961e983caf1d1e216ebe9d989ea80
Bloody Yasu <Bloody Yasu@DESKTOP-S8PQKTA>
2017-06-24 10:19:39 -0700

    my first one

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws UnknownHostException {
+        System.out.println("Hello World! Start now");
+        System.out.println("my ip adress is: "+ Inet4Address.getLocalHost());
     }
 }
